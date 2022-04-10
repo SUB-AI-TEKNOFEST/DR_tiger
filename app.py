e9671e7cd7a7717f1229658746296181e9c30483
Beytullah Aksoy <47282485+BeytullahAksoy@users.noreply.github.com>
2022-04-10 18:00:48 +0300

    show ml result in page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from flask import Flask, request, render_template
-
+from model_ml import user_diseases_list
 app = Flask(__name__)
 
 import csv
@@ -22,8 +22,10 @@ def index():
 @app.route("/predict", methods=['GET', 'POST'])
 def predict():
     input_data = list(request.form.values())
-    print(input_data)
-    return render_template('second.html')
+    print(f"1 : {[input_data[0][1:len(input_data[0])]]}")
+    result_diseases = user_diseases_list(diseases_list = [input_data[0][2:len(input_data[0])-2]])
+    print(f"5 {result_diseases}")
+    return render_template('second.html',colours = result_diseases)
 
 
 if __name__ == '__main__':
