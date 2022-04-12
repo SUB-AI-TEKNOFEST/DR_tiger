f44391ad13dc245c5d43459d89d34399c1288f0a
Beytullah Aksoy <47282485+BeytullahAksoy@users.noreply.github.com>
2022-04-12 20:39:37 +0300

    fix some bug

--- app.py
+++ app.py
@@ -26,16 +26,22 @@ def index():
         colours =data)
 
 
-@app.route("/predict", methods=['GET', 'POST'])
+@app.route('/', methods =["GET", "POST"])
 def predict():
-    selected_diseases = list(request.form.values())
+    if request.method == 'POST':
+        selected_diseases = request.form['manu']
 
     print(f"selected : {selected_diseases}")
-    #predicted_compounds = get_compound_list(diseases_list = [selected_diseases[0][2:len(selected_diseases[0])-2]])
-    #topological_results = topological_link_prediction(disase_list = [selected_diseases[0][2:len(selected_diseases[0])-2]],compound_list=predicted_compounds)
-    predicted_compounds = {'Compound::DB00745': -0.00038662006, 'Compound::DB01255': -0.0004425164, 'Compound::DB01041': -0.0005766802,
-     'Compound::DB01224': -0.0006786369, 'Compound::DB00993': -0.00071947876}
-    topological_results =  {'Compound::DB00745': -0.00038662006, 'Compound::DB01255': -0.0004425164, 'Compound::DB01041': -0.0005766802, 'Compound::DB01224': -0.0006786369, 'Compound::DB00993': -0.00071947876}
+
+    print(f"given disease  : {[selected_diseases[2:len(selected_diseases)-2]]}")
+    predicted_compounds = get_compound_list(diseases_list=[selected_diseases[2:len(selected_diseases)-2]])
+    topological_results = topological_link_prediction( disase_list=[selected_diseases[2:len(selected_diseases)-2]], compound_list=predicted_compounds)
+
+
+    # predicted_compounds = {'Compound::DB00745': -0.00038662006, 'Compound::DB01255': -0.0004425164, 'Compound::DB01041': -0.0005766802,
+    #   'Compound::DB01224': -0.0006786369, 'Compound::DB00993': -0.00071947876}
+    # topological_results = {'SARS-CoV2 nsp5::DB00755': {'tg_adamic_adar': 0, 'tg_common_neighbors': 0, 'tg_preferential_attachment': 826, 'tg_total_neighbors': 827, 'tg_resource_allocation': 0}, 'SARS-CoV2 nsp5::DB11094': {'tg_adamic_adar': 0, 'tg_common_neighbors': 0, 'tg_preferential_attachment': 0, 'tg_total_neighbors': 1, 'tg_resource_allocation': 0}, 'SARS-CoV2 nsp5::DB09341': {'tg_adamic_adar': 0, 'tg_common_neighbors': 0, 'tg_preferential_attachment': 0, 'tg_total_neighbors': 1, 'tg_resource_allocation': 0}, 'SARS-CoV2 nsp5::DB04868': {'tg_adamic_adar': 0, 'tg_common_neighbors': 0, 'tg_preferential_attachment': 0, 'tg_total_neighbors': 1, 'tg_resource_allocation': 0}, 'SARS-CoV2 nsp5::DB00815': {'tg_adamic_adar': 0, 'tg_common_neighbors': 0, 'tg_preferential_attachment': 1, 'tg_total_neighbors': 2, 'tg_resource_allocation': 0}, 'SARS-CoV2 nsp5::DB01222': {'tg_adamic_adar': 0, 'tg_common_neighbors': 0, 'tg_preferential_attachment': 4, 'tg_total_neighbors': 5, 'tg_resource_allocation': 0}, 'SARS-CoV2 nsp5::DB12291': {'tg_adamic_adar': 0.38956, 'tg_common_neighbors': 1, 'tg_preferential_attachment': 3, 'tg_total_neighbors': 3, 'tg_resource_allocation': 0.00271}, 'SARS-CoV2 nsp5::DB14681': {'tg_adamic_adar': 0, 'tg_common_neighbors': 0, 'tg_preferential_attachment': 0, 'tg_total_neighbors': 1, 'tg_resource_allocation': 0}, 'SARS-CoV2 nsp5::DB00091': {'tg_adamic_adar': 0, 'tg_common_neighbors': 0, 'tg_preferential_attachment': 2258, 'tg_total_neighbors': 2259, 'tg_resource_allocation': 0}, 'SARS-CoV2 nsp5::DB00635': {'tg_adamic_adar': 0, 'tg_common_neighbors': 0, 'tg_preferential_attachment': 1457, 'tg_total_neighbors': 1458, 'tg_resource_allocation': 0}}
+
 
     print(f"ml model {predicted_compounds}")
     print(f" topological {topological_results}")
