2432de09e42e3516ee367b746b00bb9f652e96af
utku saglam <saglmutku@gmail.com>
2022-04-13 00:27:25 +0300

    general issues fixed

--- app.py
+++ app.py
@@ -9,12 +9,14 @@ import csv
 with open('diseases_names.csv', newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
-# if os.path.exists("templates/visualization.html"):
-#   os.remove("templates/visualization.html")
-#   print("visualization deleted.")
-#
-# else:
-#   print("visualization does not exist.")
+    
+if os.path.exists("templates/visualization.html"):
+  os.remove("templates/visualization.html")
+  print("visualization deleted.")
+
+else:
+  print("visualization does not exist.")
+  
 @app.route('/')
 def index():
     return render_template(
@@ -35,18 +37,15 @@ def predict():
         topological_result_array_form.append(key_str)
         for v in value_str:
             topological_result_array_form.append(v)
-
     print(f"ml model {predicted_compounds}")
     print(f" topological {topological_results}")
     print(f" len is {len(topological_results)}")
     html_saved(disase_list= [selected_diseases[2:len(selected_diseases)-2]],compound_list=predicted_compounds)
     return render_template('second.html',ml_model = predicted_compounds,topo = topological_result_array_form ,selected = selected_diseases)
 
-
 @app.route('/visualize', methods =["GET", "POST"])
 def visualize():
     return render_template('visualization.html')
-
 
 if __name__=='__main__':
     app.run('0.0.0.0', port=5000)
