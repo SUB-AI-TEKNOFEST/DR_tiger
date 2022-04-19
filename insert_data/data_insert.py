88166a532580e5f6d2af1259eb6448796b2bfad0
Utku Saglam <58150504+utkusaglm@users.noreply.github.com>
2022-04-19 21:06:59 +0300

    data_insert modificated

--- insert_data/data_insert.py
+++ insert_data/data_insert.py
@@ -2,6 +2,7 @@ import pyTigerGraph as tg
 import pandas as pd
 import sys
 sys.path.append("..")
+
 secret="ji08rpup1652lum47ojvpuei1ievolum"
 host="https://offlabel.i.tgcloud.io"  
 graphname="DRKG"
@@ -50,19 +51,9 @@ for triplet in triplets:
     if not t in entity_dic[t_type]:
         entity_dic[t_type][t_id] = t
 
-# schema = ""
-# for entity_type in entity_dic.keys():
-#     schema += f"CREATE VERTEX {entity_type} (PRIMARY_ID Id STRING) With primary_id_as_attribute=\"true\"\n"
-# for endpoints in rtypes:
-#     [source_name, target_name] = endpoints.split("::")
-#     for edge_name in rtypes[endpoints]:
-#         schema += f"CREATE DIRECTED EDGE {edge_name} (FROM {source_name}, TO {target_name})\n"
-
-#TODO:tek tek çalıştırısak açmak lazım
-# print(conn.gsql(f"""use global {schema}"""))
 
 conn.getSchema()
-for i in range(885945,len(triplets)):
+for i in range(0,len(triplets)):
     [h,r,t] = triplets[i]
     h_type = h.split("::")[0].replace(" " ,"")
     h_id = str(h.split("::")[1])
